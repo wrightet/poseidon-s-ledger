5a2fe034c439331365b9f280d1d0f49bd2554b96
Ethan Wright <wrightet707@gmail.com>
2020-07-22 15:25:59 -0700

    remove node modules folder not used. create user controller and view

--- poseidon_s_ledger/config/routes.rb
+++ poseidon_s_ledger/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: {format: :json} do
+  end
   root 'static_pages#root'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
